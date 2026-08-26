a2135640ab55f41a8500fb2df0974742491515e4
Ivan Shahotska <107627950+SosokSlona@users.noreply.github.com>
2026-08-26 15:09:51 +0000

    fix(cloudflare): fetch /404/index.html in functions/404.js to avoid 307 redirect cycle

--- functions/404.js
+++ functions/404.js
@@ -1,11 +1,16 @@
 export async function onRequest(context) {
   const { request, env } = context;
-  const url = new URL(request.url);
-  const errorPage = await env.ASSETS.fetch(new Request(new URL('/404.html', url.origin)));
-  return new Response(errorPage.body, {
-    status: 404,
-    headers: {
-      'content-type': 'text/html; charset=utf-8'
+  try {
+    const errorPage = await env.ASSETS.fetch(new Request(new URL('/404/index.html', request.url)));
+    if (errorPage.ok) {
+      return new Response(errorPage.body, {
+        status: 404,
+        headers: {
+          'content-type': 'text/html; charset=utf-8'
+        }
+      });
     }
-  });
+  } catch (e) {}
+
+  return new Response('404 Not Found', { status: 404 });
 }
